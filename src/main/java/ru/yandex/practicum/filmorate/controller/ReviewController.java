b8f00fdb70c93be3e0c8fadf43424d33de37c389
Sunstriker <sunstrikersenpai@gmail.com>
2025-05-27 03:52:04 +0300

    Функциональность «Отзывы» добавлена

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.service.ReviewService;
--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -13,6 +13,7 @@ import ru.yandex.practicum.filmorate.model.Review;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -48,12 +49,8 @@ public class ReviewDbStorage implements ReviewStorage {
 
     @Override
     public Review updateReview(Review review) {
-        String sql = "UPDATE reviews SET is_positive = ?, content = ? WHERE user_id = ? AND film_id = ?";
-        jdbcTemplate.update(sql,
-                review.getIsPositive(),
-                review.getContent(),
-                review.getUserId(),
-                review.getFilmId());
+        String sql = "UPDATE reviews SET is_positive = ?, content = ? WHERE review_id = ?";
+        jdbcTemplate.update(sql, review.getIsPositive(), review.getContent(), review.getReviewId());
 
         return review;
     }
@@ -68,32 +65,114 @@ public class ReviewDbStorage implements ReviewStorage {
     }
 
     @Override
-    public Review getReview(Long id) {
-        return null;
+    public Optional<Review> getReview(Long id) {
+        String sql = "SELECT * FROM reviews WHERE review_id = ?";
+        List<Review> reviews = jdbcTemplate.query(sql, reviewRowMapper, id);
+        return reviews.isEmpty() ? Optional.empty() : Optional.of(reviews.getFirst());
     }
 
     @Override
     public List<Review> getReviews(Long filmId, Integer count) {
-        return List.of();
+        String sql;
+        Object[] param;
+
+        if (filmId == null) {
+            sql = "SELECT * FROM reviews ORDER BY useful DESC LIMIT ?";
+            param = new Object[]{count};
+        } else {
+            sql = sql = "SELECT * FROM reviews WHERE film_id = ? ORDER BY useful DESC LIMIT ?";
+            param = new Object[]{filmId, count};
+        }
+
+        return jdbcTemplate.query(sql, reviewRowMapper, param);
     }
 
     @Override
     public void addLike(Long reviewId, Long userId) {
+        reviewExist(reviewId);
 
+        Boolean currentVote = getCurrentVote(reviewId, userId);
+
+        if (currentVote == null) {
+            insertVote(reviewId, userId, true, 1);
+        } else if (!currentVote) {
+            setVote(reviewId, userId, true, 2);
+        }
+    }
+
+
+    @Override
+    public void removeLike(Long reviewId, Long userId) {
+        reviewExist(reviewId);
+
+        Boolean currentVote = getCurrentVote(reviewId, userId);
+
+        if (Boolean.TRUE.equals(currentVote)) {
+            deleteVote(reviewId, userId, -1);
+        }
     }
 
     @Override
     public void addDislike(Long reviewId, Long userId) {
+        reviewExist(reviewId);
 
-    }
+        Boolean currentVote = getCurrentVote(reviewId, userId);
 
-    @Override
-    public void removeLike(Long reviewId, Long userId) {
-
+        if (currentVote == null) {
+            insertVote(reviewId, userId, false, -1);
+        } else if (currentVote) {
+            setVote(reviewId, userId, false, -2);
+        }
     }
 
     @Override
     public void removeDislike(Long reviewId, Long userId) {
+        reviewExist(reviewId);
 
+        Boolean currentVote = getCurrentVote(reviewId, userId);
+
+        if (Boolean.FALSE.equals(currentVote)) {
+            deleteVote(reviewId, userId, 1);
+        }
+    }
+
+    private void reviewExist(Long reviewId) {
+        Optional<Review> review = getReview(reviewId);
+        if (review.isEmpty()) {
+            throw new NotFoundException("Review not found");
+        }
+    }
+
+    private void setVote(Long reviewId, Long userId, boolean isPositive, int usefulDelta) {
+        String updateSql = "UPDATE review_likes SET is_positive = ? WHERE review_id = ? AND user_id = ?";
+        jdbcTemplate.update(updateSql, isPositive, reviewId, userId);
+
+        String updateUsefulSql = "UPDATE reviews SET useful = useful + ? WHERE review_id = ?";
+        jdbcTemplate.update(updateUsefulSql, usefulDelta, reviewId);
+    }
+
+    private void insertVote(Long reviewId, Long userId, boolean isPositive, int usefulDelta) {
+        String insertSql = "INSERT INTO review_likes (review_id, user_id, is_positive) VALUES (?, ?, ?)";
+        jdbcTemplate.update(insertSql, reviewId, userId, isPositive);
+
+        String updateUsefulSql = "UPDATE reviews SET useful = useful + ? WHERE review_id = ?";
+        jdbcTemplate.update(updateUsefulSql, usefulDelta, reviewId);
+    }
+
+    private void deleteVote(Long reviewId, Long userId, int usefulDelta) {
+        String deleteSql = "DELETE FROM review_likes WHERE review_id = ? AND user_id = ?";
+        jdbcTemplate.update(deleteSql, reviewId, userId);
+
+        String updateUsefulSql = "UPDATE reviews SET useful = useful + ? WHERE review_id = ?";
+        jdbcTemplate.update(updateUsefulSql, usefulDelta, reviewId);
+    }
+
+    private Boolean getCurrentVote(Long reviewId, Long userId) {
+        String selectSql = "SELECT is_positive FROM review_likes WHERE review_id = ? AND user_id = ?";
+        return jdbcTemplate.query(
+                selectSql,
+                rs -> rs.next() ? rs.getBoolean("is_positive") : null,
+                reviewId, userId
+        );
     }
 }
