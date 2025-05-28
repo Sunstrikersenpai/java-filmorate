bdac592fad2d9bf9f335941f29133f425e671bc3
Sunstriker <sunstrikersenpai@gmail.com>
2025-05-28 17:13:59 +0300

    \

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -28,6 +28,6 @@ public class Film {
     @Positive(message = "Продолжительность должна быть больше 0")
     private Integer duration;
     private Set<Long> usersLikes = new HashSet<>();
-    private Set<Genre> genres;
+    private Set<Genre> genres = new HashSet<>();
     private Mpa mpa;
 }
